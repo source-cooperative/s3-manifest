3371b9f2701f00845675ae5a54576baacf7c92de
Kevin Booth <kevin@kb.gg>
2024-08-20 12:46:45 -0500

    Removed scripts

--- scripts/build-push.sh
+++
@@ -1,4 +0,0 @@
-VERSION=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[0].version')
-docker buildx build --platform linux/arm64 -t 417712557820.dkr.ecr.us-west-2.amazonaws.com/source-s3-manifest:v$VERSION --push .
-aws ecr get-login-password --region us-west-2 --profile opendata | docker login --username AWS --password-stdin 417712557820.dkr.ecr.us-west-2.amazonaws.com
-docker push 417712557820.dkr.ecr.us-west-2.amazonaws.com/source-s3-manifest:v$VERSION
